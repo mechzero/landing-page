0a210285aff592ae6fc3d2486c34a4a6bcaecd01
Sarah Maris <sarah.maris@udacity.com>
2022-06-21 23:09:00 -0400

    Adjust formatting

--- starter/src/assets/front.js
+++ starter/src/assets/front.js
@@ -2,12 +2,11 @@ let currencySymbol = '$';
 
 // Draws product list
 function drawProducts() {
-    
     let productList = document.querySelector('.products');
     let productItems = '';
-    products.forEach(element => {
-        productItems +=`
-            <div data-SKU='${element.SKU}'>
+    products.forEach((element) => {
+        productItems += `
+            <div data-productId='${element.productId}'>
                 <img src='${element.image}'>
                 <h3>${element.name}</h3>
                 <p>price: ${currencySymbol}${element.price}</p>
@@ -24,11 +23,11 @@ function drawCart() {
     let cartList = document.querySelector('.cart');
     // clear cart before drawing
     let cartItems = '';
-    cart.forEach(element => {
+    cart.forEach((element) => {
         let itemTotal = element.price * element.quantity;
 
-        cartItems +=`
-            <div data-SKU='${element.SKU}'>
+        cartItems += `
+            <div data-productId='${element.productId}'>
                 <h3>${element.name}</h3>
                 <p>price: ${currencySymbol}${element.price}</p>
                 <p>quantity: ${element.quantity}</p>
@@ -39,20 +38,22 @@ function drawCart() {
             </div>
         `;
     });
-     // use innerHTML so that cart products only drawn once
-    cart.length ? cartList.innerHTML = cartItems : cartList.innerHTML = 'Cart Empty';
+    // use innerHTML so that cart products only drawn once
+    cart.length
+        ? (cartList.innerHTML = cartItems)
+        : (cartList.innerHTML = 'Cart Empty');
 }
 
 // Draws checkout
 function drawCheckout() {
     let checkout = document.querySelector('.cart-total');
-    checkout.innerHTML ='';
+    checkout.innerHTML = '';
 
     // run cartTotal() from script.js
     let cartSum = cartTotal();
 
-    let div = document.createElement("div");
-    div.innerHTML =`<p>Cart Total: ${currencySymbol}${cartSum}`;
+    let div = document.createElement('div');
+    div.innerHTML = `<p>Cart Total: ${currencySymbol}${cartSum}`;
     checkout.append(div);
 }
 
@@ -62,27 +63,25 @@ drawCart();
 drawCheckout();
 
 document.querySelector('.products').addEventListener('click', (e) => {
-    let productSKU = e.target.parentNode.getAttribute('data-SKU');
-    productSKU *= 1;
-    addProductToCart(productSKU);     
+    let productId = e.target.parentNode.getAttribute('data-productId');
+    productId *= 1;
+    addProductToCart(productId);
     drawCart();
     drawCheckout();
-})
-
+});
 
 // Event delegation used to support dynamically added cart items
 document.querySelector('.cart').addEventListener('click', (e) => {
-    
     // Helper nested higher order function to use below
     // Must be nested to have access to the event target
     // Takes in a cart function as an agrument
     function runCartFunction(fn) {
-        let productSKU = e.target.parentNode.getAttribute('data-SKU');
-        productSKU *= 1;
+        let productId = e.target.parentNode.getAttribute('data-productId');
+        productId *= 1;
         for (let i = cart.length - 1; i > -1; i--) {
-            if (cart[i].SKU === productSKU) {
-                let SKU = cart[i].SKU;
-                fn(SKU);
+            if (cart[i].productId === productId) {
+                let productId = cart[i].productId;
+                fn(productId);
             }
         }
         // force cart and checkout redraw after cart function completes
@@ -91,17 +90,17 @@ document.querySelector('.cart').addEventListener('click', (e) => {
     }
 
     // check the target's class and run function based on class
-    if (e.target.classList.contains('remove')){
+    if (e.target.classList.contains('remove')) {
         // run removeProductFromCart() from script.js
         runCartFunction(removeProductFromCart);
-    } else if (e.target.classList.contains('qup')){
+    } else if (e.target.classList.contains('qup')) {
         // run increaseQuantity() from script.js
         runCartFunction(increaseQuantity);
-    } else if (e.target.classList.contains('qdown')){
+    } else if (e.target.classList.contains('qdown')) {
         // run decreaseQuantity() from script.js
         runCartFunction(decreaseQuantity);
-    } 
-})
+    }
+});
 
 document.querySelector('.pay').addEventListener('click', (e) => {
     e.preventDefault();
@@ -112,22 +111,22 @@ document.querySelector('.pay').addEventListener('click', (e) => {
 
     // Set cashReturn to return value of pay()
     let cashReturn = pay(amount);
-    
+
     let paymentSummary = document.querySelector('.pay-summary');
-    let div = document.createElement("div");
+    let div = document.createElement('div');
 
     // If total cash received is greater than cart total thank customer
     // Else request additional funds
-    if (cashReturn >= 0 ){
-        div.innerHTML =`
+    if (cashReturn >= 0) {
+        div.innerHTML = `
             <p>Cash Received: ${currencySymbol}${amount}</p>
             <p>Cash Returned: ${currencySymbol}${cashReturn}</p>
             <p>Thank you!</p>
         `;
     } else {
         // reset cash field for next entry
-        document.querySelector('.received').value = ''; 
-        div.innerHTML =`
+        document.querySelector('.received').value = '';
+        div.innerHTML = `
             <p>Cash Received: ${currencySymbol}${amount}</p>
             <p>Remaining Balance: ${cashReturn}$</p>
             <p>Please pay additional amount.</p>
@@ -136,8 +135,7 @@ document.querySelector('.pay').addEventListener('click', (e) => {
     }
 
     paymentSummary.append(div);
-})
-
+});
 
 /* Standout suggestions */
 /* Begin remove all items from cart */
@@ -191,4 +189,3 @@ document.querySelector('.pay').addEventListener('click', (e) => {
 // });
 /* End currency converter */
 /* End standout suggestions */
-
